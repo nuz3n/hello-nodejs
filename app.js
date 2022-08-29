81ba2b714884ff10362ef7398af49d2585dbfddb
Zen <niti.lembaroheem@gmail.com>
2022-08-29 15:45:48 +0700

    update code

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const http = require('node:http');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+app.get("/", (req, res) => {
+  res.send("Hello! Node.js");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log("Starting node.js at port " + port);
 });
